2b9ad57844f5b8ed2fcb6a49df93c7d15f2414e3
awsker <awsker@outlook.com>
2017-11-08 18:33:44 +0100

    Whoops

--- conc/Program.cs
+++ conc/Program.cs
@@ -14,20 +14,9 @@ namespace conc
         /// </summary>
         [STAThread]
         static void Main()
-        {
-            /*
+        { 
             using (var game = new Main())
                 game.Run();
-            */
-            var line1 = new Line(Vector2.Zero, new Vector2(-2f, -2f), LineType.InfiniteAtTarget);
-            var line2 = new Line(new Vector2(4f, 0f), new Vector2(4f, 2f), LineType.BothCapped);
-
-            Vector2 closest;
-            for (int i = 0; i < 100000; ++i)
-            {
-                line1.Intersecting(line2);
-            }
-            var time = Line.Stop.ElapsedMilliseconds;
         }
     }
 }
